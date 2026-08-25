1789639357f3847865139e42762808433bdcd223
LucaasSantos <148167204+LucaasSantos@users.noreply.github.com>
2026-08-25 17:05:52 -0300

    Upgrade cache version and implement cleanup
    Updated cache name to version 2 and added cache cleanup for old versions.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trackm7-cache-v1';
+const CACHE_NAME = 'trackm7-cache-v2'; // MUDAMOS PARA V2 AQUI!
 const urlsToCache = [
   './',
   './index.html',
@@ -10,11 +10,27 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+// Limpa os caches antigos (como a versão v1)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
